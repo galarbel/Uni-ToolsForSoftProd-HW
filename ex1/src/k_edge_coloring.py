daf24e421446f9974dd7cacff82e67019a5dc830
galarbel <gal.arbel88@gmail.com>
2016-11-13 00:16:47 +0200

    1a done \o/

--- ex1/src/k_edge_coloring.py
+++ ex1/src/k_edge_coloring.py
@@ -1,6 +1,9 @@
 """
 k-edge-coloring exercise.
 """
+from z3 import *
+
+DEBUG = False
 
 Petersen_V = range(10)
 Petersen_E = [
@@ -25,10 +28,58 @@ Petersen_E = [
 
 
 def get_k_edge_coloring(k, V, E):
-    #
-    # Your solution here...
-    #
-    pass
+    assert V == range(len(V))
+    colors = range(k)
+    EdgeNumbers = range(len(E))
+
+    variables = [[Bool('e_{}_color_{}'.format(e, c)) for c in colors] for e in EdgeNumbers]
+
+    s = Solver()
+
+    # every edge has at least one color
+    for e in EdgeNumbers:
+        s.add(Or([variables[e][c] for c in colors]))
+
+    # every edge has at most one color
+    for e in EdgeNumbers:
+        for c1 in range(k):
+            for c2 in range(c1 + 1, k):
+                s.add(Or(Not(variables[e][c1]),
+                         Not(variables[e][c2])))
+
+    # every edge has different colors from it's neighbours
+    for e in EdgeNumbers:
+        v1, v2 = E[e]
+        for i in range(e+1, len(EdgeNumbers)):
+            if (v1 in E[i]) or (v2 in E[i]):
+                for c in colors:
+                    s.add(Or(Not(variables[e][c]),
+                             Not(variables[i][c])))
+    if DEBUG:
+        print "Solver is:"
+        print s
+        print
+
+        print "Checking SAT"
+    res = s.check()
+
+    if res == unsat or res == unknown:
+        if DEBUG:
+            print("UNSAT, No K coloring" if res == unsat else "UNKNOWN")
+        return None
+
+    assert res == sat
+    if DEBUG:
+        print "SAT, Found K coloring"
+    m = s.model()
+    coloring = dict()
+    for e in EdgeNumbers:
+        for c in colors:
+            if is_true(m[variables[e][c]]):
+                coloring[E[e]] = c
+                break
+    return coloring
+
 
 
 def get_k_edge_coloring_core(k, V, E):
@@ -64,7 +115,16 @@ def draw_graph(V, E, coloring={}, filename='graph', engine='circo', directed=Fal
 
 
 if __name__ == '__main__':
-    #
-    # Your tests here...
-    #
+
+    print("Peterson 4 col:")
+    print(get_k_edge_coloring(4,Petersen_V,Petersen_E))
+    print
+
+    print("Peterson 3 col:")
+    print(get_k_edge_coloring(3, Petersen_V, Petersen_E))
+    print
+
+    print("simple test graph: k=2 V=[0,1,2,3], E= [(0, 1), (0,2)]")
+    print(get_k_edge_coloring(2, range(4), [(0, 1), (0, 2)]))
+    print
     pass
