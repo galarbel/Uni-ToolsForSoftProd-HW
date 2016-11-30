d09191a7ec761d5c992b37f50607b8906039eeed
nabeelhod <nabeel.hod@gmail.com>
2016-11-30 22:15:01 +0200

    Adding more bugs & fixes

--- ex2/comp_rem.c
+++ ex2/comp_rem.c
@@ -1,12 +1,13 @@
 int compute_remainder(int x, int y) 
 {
 	int remainder;
-	if(y!=0)
-	{
+
+	if(y!=0){
 		remainder = x % y;
 		return remainder;
 	}
-
-	printf("can't divide by zero");
-	return;
+	else{
+		printf("can't divide by zero");
+		return;
+	}
 }
