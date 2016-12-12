b8935d74ac296e54861bea14e017e655cc34615e
aniskharouba <k.anees93@gmail.com>
2016-12-13 00:41:53 +0200

    finish bubble sort, adding makefile

--- ex3/sort.c
+++ ex3/sort.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
 	int num_of_elements;
-	int element;
+	int element,tmp;
 	int *arr;
 	
 	if(argc != 1)
@@ -21,18 +22,39 @@ int main(int argc, char *argv[])
 	
 	arr = malloc(sizeof(int)*num_of_elements);
 	
-	for(int i=0;i<num_of_elemnts;i++)
+	for(int i=0;i<num_of_elements;i++)
 	{
-		printf("please enter the element %d",i);
+		printf("please enter the element %d: ",i);
 		if(scanf("%d",&element) != 1)
 		{
 			free(arr);
 			return -1; 
 		}
-		a[i] = element;
+		arr[i] = element;
 	}
 	
-	//implement merge sort.
+	 for (int i = 0; i < num_of_elements - 1; ++i)
+	 {
+		  for (int j = 0; j < num_of_elements - 1 - i; ++j )
+		  {
+			   if (arr[j] > arr[j+1])
+			   {
+					tmp = arr[j+1];
+					arr[j+1] = arr[j];
+					arr[j] = tmp;
+			   }
+		  }
+	 }
+	
+	printf("\nThe array sorted.\n");
+	for(int i=0; i< num_of_elements;++i)
+	{
+		printf("%d ",arr[i]);
+	}
+	printf("\n");
+	free(arr);
+	
+	return 1;
 	
 }
 
